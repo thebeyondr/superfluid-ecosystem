6fa0b907fa28a660ddf64216571b83831ede7f1d
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-08-09 18:48:34 -0500

    feat: :speech_balloon: Add links to wrapper and token wizard

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
-import Image from 'next/image'
+import Link from "next/link";
 
 export default function Home() {
-  return (
-      <main className="flex min-h-full flex-col items-center justify-between p-24">
-          <h1 className="text-stone-50 text-[40px] font-normal">Astryd</h1>
-      </main>
-  );
+    const links = {
+        toDeployer: "https://deploy-supertoken-deployment.vercel.app/",
+        toWizard: "https://supertoken-wizard.huntersworkshop.xyz/",
+    };
+    return (
+        <main className="flex min-h-full flex-col items-center justify-between p-6 lg:p-24 space-y-2">
+            <h1 className="text-stone-50 text-[40px] font-normal">Astryd</h1>
+            <p className="text-sm text-stone-50/60 font-medium">
+                Supertoken Merchant
+            </p>
+            <Link
+                href={links.toDeployer}
+                target={"_blank"}
+                className="p-6 flex flex-col items-center justify-center w-full bg-stone-800 hover:bg-stone-600 transition"
+            >
+                Wrap token
+            </Link>
+            <Link
+                href={links.toWizard}
+                target={"_blank"}
+                className="p-6 flex flex-col items-center justify-center w-full bg-stone-800 hover:bg-stone-600 transition"
+            >
+                Spawn pure token
+            </Link>
+            <div className="p-6 flex flex-col items-center justify-center w-full bg-stone-800 hover:bg-stone-600 transition">
+                Donate
+            </div>
+        </main>
+    );
 }
