b62798306e892cb3e7f1a2fdc2facbccaaea03b8
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-08-11 23:40:08 -0500

    refactor: :iphone: Update responsive styles
    Add another container thats also screen-height to offset the effects of `ThemeProvider`

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,21 +29,23 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <body className="min-h-screen flex flex-col bg-astryd-mobile md:bg-astryd-desktop bg-cover bg-top-right md:bg-center bg-no-repeat">
+            <body className="h-screen flex flex-col bg-astryd-mobile md:bg-astryd-desktop bg-cover bg-top-right md:bg-center bg-no-repeat">
                 <ThemeProvider attribute="class" defaultTheme="dark">
-                    <nav className="p-6 lg:p-12 flex space-x-2 lg:space-x-3 items-center">
-                        <div className="h-10 w-10 lg:h-14 lg:w-14 relative">
-                            <Image
-                                src={"/assets/graphics/hw-logo-white.svg"}
-                                alt="Hunters Workshop Logo"
-                                fill
-                            />
-                        </div>
-                        <p className="text-xs lg:text-sm text-stone-50 tracking-wider leading-[15.12px] lg:leading-tight uppercase font-bold">
-                            Hunter&apos;s <br /> Workshop
-                        </p>
-                    </nav>
-                    {children}
+                    <div className="h-screen flex flex-col">
+                        <header className="p-6 lg:p-12 flex space-x-2 lg:space-x-3 items-center">
+                            <div className="h-10 w-10 lg:h-14 lg:w-14 relative">
+                                <Image
+                                    src={"/assets/graphics/hw-logo-white.svg"}
+                                    alt="Hunters Workshop Logo"
+                                    fill
+                                />
+                            </div>
+                            <p className="text-xs lg:text-sm text-stone-50 tracking-wider leading-[15.12px] lg:leading-tight uppercase font-bold">
+                                Hunter&apos;s <br /> Workshop
+                            </p>
+                        </header>
+                        {children}
+                    </div>
                 </ThemeProvider>
             </body>
         </html>
